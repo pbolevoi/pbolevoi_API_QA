214cdaaebac4c6caf35d03f16fdd32822ea70def
pbolevoi <96445356+pbolevoi@users.noreply.github.com>
2026-06-22 21:53:06 +0300

    Delete example.py

--- example.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-HOST = 'https://release-gs.qa-playground.com/api/v1'
-
-response = requests.post(
-    url=f'{HOST}/setup',
-    headers={'Authorization': 'Bearer iMTQ4ZC1LY2MOLTQ4ODctOGJiZCALMTYZYikxNZA3NDIifl.KXZuNHXp66RiDdmXZXNi-Q16n3KvLaPf5s-jJvd4'}
-)
